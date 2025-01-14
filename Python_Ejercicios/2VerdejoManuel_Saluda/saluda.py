cfee1c9d0a170d62d93d2d38c8e2bc95d406ff4c
ManuelVR03 <manuel.verdejo.ramirez.alu@iesjulioverne.es>
2025-01-14 09:27:51 +0100

    Agregar función de saludo personalizada y pruebas en saluda.py y saluda_test.py

---
+++ Python_Ejercicios/2VerdejoManuel_Saluda/saluda.py
@@ -0,0 +1,5 @@
+def saluda(nombre):
+    '''
+    Esta función imprime un saludo personaliado con el nombre que se le pase como argumento.
+    '''
+    print("Hola " + nombre)
---
+++ Python_Ejercicios/2VerdejoManuel_Saluda/saluda_test.py
@@ -0,0 +1,11 @@
+from saluda import *
+
+def main():
+    saluda("Manuel")
+    saluda("Lucía")
+    saluda("Patri")
+    saluda("Tina")
+    help(saluda)
+    
+if __name__ == "__main__":
+    main()
